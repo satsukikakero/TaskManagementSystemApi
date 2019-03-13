2e7e93172f124e24822dc5a23cb4cecdb1c35450
Yavor Vasilev <abubu@abv.bg>
2019-03-13 22:02:44 +0200

    Fix IApiModel got delete so it had to be created again

---
+++ TMSApi/ApiModels/IApiModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMSApi.ApiModels
+{
+    public interface IApiModel
+    {
+    }
+}
