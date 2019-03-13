d446b2f7fdc59688b0ca2b2cd696b09194cf9552
Yavor Vasilev <abubu@abv.bg>
2019-03-13 21:10:51 +0200

    Added Api models for comments and task

---
+++ TMSApi/ApiModels/ApiComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMSApi.ApiModels
+{
+    public class ApiComment
+    {
+        public DateTime DateAdded { get; set; }
+        public string CommentText { get; set; }
+        public string CommentType { get; set; }
+        public DateTime? ReminderDate { get; set; }
+    }
+}
---
+++ TMSApi/ApiModels/ApiTask.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMSApi.ApiModels
+{
+    public class ApiTask
+    {
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime RequestByDate { get; set; }
+        public List<User> AssignedTo { get; set; }
+        public DateTime NextActionDate { get; set; }
+    }
+}
